91b3e6aa805e5d216340fc818772e504d8ace58f
Mathias Polo <mathiaspolo93@gmail.com>
2022-12-02 20:42:05 +0100

    exercise_division.R: group exs by topic

--- homework_3/exercise_division.R
+++ homework_3/exercise_division.R
@@ -1,8 +1,8 @@
+LAB_exs = 1
 FSDS_exs = c(4.24, 4.26, 4.62,
              "6.10", "6.30", 6.42, 6.52) #quotes to avoid stripping trailing zero
-LAB_exs = 1
 
-books = c(rep("FSDS", length(FSDS_exs)), rep("LAB", length(LAB_exs)))
+books = c(rep("LAB", length(LAB_exs)), rep("FSDS", length(FSDS_exs)))
 
 students = c()
 set.seed(? + ? + ? + ?) #days of birth
@@ -12,7 +12,7 @@ for (i in 1:2) {
 
 df = data.frame(student = students[1:length(books)],
                 book = books,
-                exercise = c(FSDS_exs, LAB_exs))
+                exercise = c(LAB_exs, FSDS_exs))
 df = df[order(df$student),]
 rownames(df) = NULL
 print(df)
