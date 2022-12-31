c6040805ced574695ac3581fd40b6501a40fef22
Mathias Polo <mathiaspolo93@gmail.com>
2022-12-31 14:10:01 +0100

    exercise_division.R: add days of birth

--- homework_4/exercise_division.R
+++ homework_4/exercise_division.R
@@ -3,7 +3,7 @@ FSDS_exs = c(7.4, "7.20", 7.26, 7.28, 7.42, 7.44) #quotes to avoid stripping tra
 books = rep("FSDS", length(FSDS_exs))
 
 students = c()
-set.seed(? + ? + ?) #days of birth
+set.seed(29 + 3 + 6) #days of birth
 for (i in 1:2) {
   students = c(students, sample(c("S. Lippolis", "F. Spreafichi", "M. Polo")))
 }
